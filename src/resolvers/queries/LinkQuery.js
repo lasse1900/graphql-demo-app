b7c1487a0ac0bd89ef23875d4ad53305495e1bdb
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-03-11 12:45:10 +0200

    done some code refactoring, sanitazing to resolvers

---
+++ src/resolvers/queries/LinkQuery.js
@@ -0,0 +1,14 @@
+
+
+function linksFeed(parent, args, ctx, info) {
+  return ctx.db.query.links({}, info)
+}
+
+function singleLink(parent, { id }, ctx, info) {
+  return ctx.db.query.link({ where: { id: id } }, info)
+}
+
+module.exports = {
+  linksFeed,
+  singleLink
+}
